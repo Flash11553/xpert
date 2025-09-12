e79be8d15241b83ea17facb87d1be95e3727635d
Flash11553 <flash.14599@gmail.com>
2025-09-12 23:17:55 +0400

    Update play.py

--- BrandrdXMusic/utils/inline/play.py
+++ BrandrdXMusic/utils/inline/play.py
@@ -71,7 +71,7 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
                 text="⛦ ᴋᴜʀᴜᴄᴜ ❥", url=f"https://t.me/Legnddi"
             ),
             InlineKeyboardButton(
-                text="⛦ sᴜᴘᴘᴏʀᴛ ❥", url=f"https://t.me/Xpert_Support"
+                text="⛦ sᴜᴘᴘᴏʀᴛ ❥", url=f"https://t.me/Vipbotlarsupport"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -93,7 +93,7 @@ def stream_markup(_, videoid, chat_id):
                 text="⛦ ᴋᴜʀᴜᴄᴜ ❥", url=f"https://t.me/Legnddi"
             ),
             InlineKeyboardButton(
-                text="⛦ sᴜᴘᴘᴏʀᴛ ❥", url=f"https://t.me/Xpert_Support"
+                text="⛦ sᴜᴘᴘᴏʀᴛ ❥", url=f"https://t.me/Vipbotlarsupport"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -248,7 +248,7 @@ def stream_markup2(_, chat_id):
                 text="⛦ ᴋᴜʀᴜᴄᴜ ❥", url=f"https://t.me/Legnddi"
             ),
             InlineKeyboardButton(
-                text="⛦ sᴜᴘᴘᴏʀᴛ ❥", url=f"https://t.me/Xpert_Support"
+                text="⛦ sᴜᴘᴘᴏʀᴛ ❥", url=f"https://t.me/Vipbotlarsupport"
             ),
         ],
 
